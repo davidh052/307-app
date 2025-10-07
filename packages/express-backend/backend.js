3b2ca292241390baac00e2c5bc97d55f3f7b52e3
david <davidh0511@hotmai.com>
2025-10-06 23:52:09 -0700

    added add user

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -74,6 +74,18 @@ app.get("/users", (req, res) => {
       res.send(result);
     }
   });
+
+
+  const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
   
   
 
